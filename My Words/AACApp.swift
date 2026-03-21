f051bb450441fce4d96908988b4e20bb7c843acb
James Baker <jbaker00@users.noreply.github.com>
2026-03-21 11:07:16 -0700

    fixed the code to ensure that we can run as debug with test ads until the ad unit is approved

--- My Words/AACApp.swift
+++ My Words/AACApp.swift
@@ -4,6 +4,9 @@ import GoogleMobileAds
 @main
 struct AACApp: App {
     init() {
+        #if DEBUG
+        MobileAds.shared.requestConfiguration.testDeviceIdentifiers = ["b25b3c0304041fc99af04bd474bfcc9a"]
+        #endif
         MobileAds.shared.start()
     }
 
